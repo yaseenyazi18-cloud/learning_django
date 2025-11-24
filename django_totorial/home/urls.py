cecf2581bdfd39ed119abfae64cfccdefb7f4cba
Muhammed Yaseen A <yaseenyazi18@gmail.com>
2025-11-24 22:27:01 +0530

    django project update-include variables,tag,and template inheritance

--- django_totorial/home/urls.py
+++ django_totorial/home/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
-    path('booking/', views.booking),
-    path('doctor/', views.doctor),
-    path('contact/', views.contact),
-    path('department/', views.department),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('booking/', views.booking, name='booking'),
+    path('doctor/', views.doctor, name='doctor'),
+    path('contact/', views.contact, name='contact'),
+    path('department/', views.department, name='department'),
        
 ]
 
