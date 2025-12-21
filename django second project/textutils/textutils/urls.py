081f08ffe54fc257fb5562e1b45bdaae6c85a0e9
Muhammed Yaseen A <yaseenyazi18@gmail.com>
2025-12-21 23:14:20 +0530

    django miniproject[textutils]

--- django second project/textutils/textutils/urls.py
+++ django second project/textutils/textutils/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('',views.index, name="home"),
     path('about',views.about, name="about"),
     path('contact',views.contact, name="contact"),
+    path('analyze',views.analyze, name="analyze"),
     
     
 ]
--- django second project/textutils/textutils/views.py
+++ django second project/textutils/textutils/views.py
@@ -1,10 +1,76 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse(""
-    "<a href='https://www.youtube.com/watch?v=84dtpjaTecQ&list=PLbasZIkCgHJGXEjcatJ3aO1NpS2PsOtoQ&index=4'>Django tutorial</a>"
-    
-    "\t<a href='https://www.youtube.com/watch?v=oxtvz_zaClw&list=PL4dPAKlZK9IRPvKk8AuG_lBQLKIoRVDi2&index=4'>Django project</a>""")
+    yazi = "Hey I'm Muhammed Yasseem"
+    hana = "Hey i'm Hana Fathima"
+    data = {'yzi':yazi,
+            'hana':hana
+    }
+    return render(request, "index.html", data)
+
+def analyze(request):
+    if request.method == "GET":
+        text = request.GET.get('textname')
+        removeponc = request.GET.get("removeponc", "off")
+        capitalize = request.GET.get("capitalize", "off")
+        lowercase = request.GET.get("lowercase", "off")
+        
+        
+        punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+        analyzed = ""
+        if removeponc == "on":
+            
+            for char in text:
+                if char not in punctuation:
+                   analyzed = analyzed + char
+                
+        if capitalize == "on":
+                analyzed = text.upper()
+
+        if lowercase == "on":
+            analyzed = text.lower()
+              
+             
+            data = { 
+                    'text':analyzed
+            }
+                   
+
+
+                  
+        else:
+            return HttpResponse("Error")
+
+
+    return render(request, "analyze.html", data)            
+        # if Capitalize == "on":
+           
+        #     for char in text:
+        #         if char not in punctuation:
+        #             analyzed = analyzed + char
+        #             captxt = analyzed.upper()
+        #         data = { 
+        #             'captxt':captxt
+        #             }
+
+        # elif Lowercase == "on":
+        #         analyzed = ""
+        #         for char in text:
+        #             if char not in punctuation:
+        #                 analyzed = analyzed + char
+        #                 captxt = analyzed.upper()
+        #                 lowtext = analyzed.lower()    
+        #             data = {
+        #                 'lowtext':lowtext 
+        #             } 
+
+        
+
+                 
+       
+
+
 
 def about(request):
     return HttpResponse("<h1>i'm yaseen. I can do it</h1>")
