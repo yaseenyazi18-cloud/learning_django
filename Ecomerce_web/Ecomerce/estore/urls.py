4ebaf1541c48971ca028d1523eb0a2741bfd0a56
Muhammed Yaseen A <yaseenyazi18@gmail.com>
2025-12-23 22:57:58 +0530

    add collections

--- Ecomerce_web/Ecomerce/estore/urls.py
+++ Ecomerce_web/Ecomerce/estore/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 
     path('',views.index, name="home"),
+    path('collections',views.collections, name="collections"),
     
    
 ]
--- Ecomerce_web/Ecomerce/estore/views.py
+++ Ecomerce_web/Ecomerce/estore/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def collections(request):
+    return render(request, 'collections.html' )
+
