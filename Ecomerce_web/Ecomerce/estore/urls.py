0005374e99f7e99026b17302d8ef093a201b8de2
Muhammed Yaseen A <yaseenyazi18@gmail.com>
2025-12-24 23:33:41 +0530

    ecomerce web site

--- Ecomerce_web/Ecomerce/estore/urls.py
+++ Ecomerce_web/Ecomerce/estore/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('',views.index, name="home"),
     path('collections',views.collections, name="collections"),
+    path('loging',views.loging, name="loging"),
+    path('register',views.register, name="register"),
     
    
 ]
--- Ecomerce_web/Ecomerce/estore/views.py
+++ Ecomerce_web/Ecomerce/estore/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,24 @@ def index(request):
 def collections(request):
     return render(request, 'collections.html' )
 
+def loging(request):
+    return render(request, 'loging.html')
+
+def register(request):
+    if request.method =='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        confirmpassword=request.POST['confirmpassword']
+        if User.objects.filter(username=username).exists():
+            messages.info(request, "username already exists")
+            return redirect('register')
+        elif user.objects.filter(email=email).exists():
+            messages.info(request, "email taken")
+            return redirect('register')
+        else:
+            user=User.objects.create_user(username==username, email==email, password==password, confirmpassword==confirmpassword )
+            user.save();                                                                                                                                                                                       
+            return redirect('/')    
+    else:
+        return render(request, 'register.html', )
