530729c30a8b37fa1bc5d9bf035d3e83f5b956c0
Muhammed Yaseen A <yaseenyazi18@gmail.com>
2025-12-28 23:19:01 +0530

    Start ORM

--- Eco_pro/ecomanage/webapp/urls.py
+++ Eco_pro/ecomanage/webapp/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/',views.create, name='create'),
+   
+    path('',views.create, name='create'),
     path('edit/',views.edit, name='edit'),
-    path('list',views.list, name='list'),
+    path('list/',views.list, name='list'),
   
   
   
--- Eco_pro/ecomanage/webapp/views.py
+++ Eco_pro/ecomanage/webapp/views.py
@@ -2,10 +2,57 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
+    if request.POST:
+        print(request.POST)
+        print(request.POST.get('name'))
+        print(request.POST.get('year'))
     return render(request,'create.html')
 
 def edit(request):
     return render(request, 'edit.html')
 
 def list(request):
-    return render(request, 'list.html')
+    lap = { 'laptop': [{
+        'name':'Lenovo IQL',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'laptops.jpg'
+        },
+        {
+        'name':'dsi',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'surtop.jpg'
+        },               
+        {
+        'name':'acer',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'lap.jpeg'
+        },
+        {
+        'name':'hp',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'surtop.jpg'
+        },
+        {
+        'name':'asuse',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'lap.jpeg'
+        },
+        {
+        'name':'IQL',
+        'spec' : ' Intel Core i7-1165G7 · Intel Iris Xe Graphics G7 (96EU) ',
+        'year' : '8 month',
+        'sucess' : True,
+        'img':'laptops.jpg'
+        },               
+        ]}
+    return render(request, 'list.html', lap)
